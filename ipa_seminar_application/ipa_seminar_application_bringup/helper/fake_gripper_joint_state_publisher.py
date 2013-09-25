02edec1477cef6c4f87f64e2d73d30fcb98d1f84
ipa-fmw <florian.weisshardt@ipa.fraunhofer.de>
2013-09-25 14:45:14 +0200

    add fake joint publisher

---
+++ ipa_seminar_application/ipa_seminar_application_bringup/helper/fake_gripper_joint_state_publisher.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('ipa_seminar_application_bringup')
+import rospy
+import time
+from sensor_msgs.msg import JointState
+
+def fake_ur_connector():
+        rospy.init_node('fake_joint_state_publisher_connector')
+
+        # emergency_stop topic
+        pub = rospy.Publisher('/joint_states',JointState)
+        msg = JointState()
+        msg.header.stamp = rospy.Time.now()
+        msg.name = ['gripper_finger_1_joint', 'gripper_finger_2_joint']
+        msg.position = [0.0, 0.0]
+        msg.velocity = [0.0, 0.0]
+        msg.effort = [0.0, 0.0]
+
+        while not rospy.is_shutdown():
+                pub.publish(msg)
+                rospy.sleep(0.1)
+
+if __name__ == '__main__':
+        try:
+                fake_ur_connector()
+        except rospy.ROSInterruptException:
+                print "Interupted"
+                pass
