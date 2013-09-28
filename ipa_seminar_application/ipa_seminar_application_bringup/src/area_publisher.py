9dcd7a8136887c785754d185e79bb86d27850a21
ipa-fmw <florian.weisshardt@ipa.fraunhofer.de>
2013-09-28 20:40:35 +0200

    fix area publisher

--- ipa_seminar_application/ipa_seminar_application_bringup/src/area_publisher.py
+++ ipa_seminar_application/ipa_seminar_application_bringup/src/area_publisher.py
@@ -13,45 +13,48 @@ def publisher():
 	pub = rospy.Publisher('areas', Marker)
 
 	while not rospy.is_shutdown():
-		color = ColorRGBA()
-		color.r = 0
-		color.g = 1
-		color.b = 0
-		color.a = 1
-		areas = rospy.get_param("/application")
+		try:
+			color = ColorRGBA()
+			color.r = 0
+			color.g = 1
+			color.b = 0
+			color.a = 1 
+			areas = rospy.get_param("/application")
 
-		for area in areas:
-			# cube
-			marker = Marker()
-			marker.header.frame_id = "/base_link"
-			marker.header.stamp = rospy.Time.now()
-			marker.pose = get_pose_from_parameter_server(area).pose
-			marker.pose.position.z = 1.0
-			marker.ns = area
-			marker.type = 1
-			marker.scale.x = 0.1
-			marker.scale.y = 0.1
-			marker.scale.z = 0.01
-			marker.color = color
-			pub.publish(marker)
+			for area in areas:
+				# cube
+				marker = Marker()
+				marker.header.frame_id = "/base_link"
+				marker.header.stamp = rospy.Time.now()
+				marker.pose = get_pose_from_parameter_server(area).pose
+				marker.pose.position.z = 1.0
+				marker.ns = area
+				marker.type = 1
+				marker.scale.x = 0.1
+				marker.scale.y = 0.1
+				marker.scale.z = 0.01
+				marker.color = color
+				pub.publish(marker)
 			
-			# text
-			text_marker = Marker()
-			text_marker.header.frame_id = "/base_link"
-			text_marker.header.stamp = rospy.Time.now()
-			text_marker.pose = get_pose_from_parameter_server(area).pose
-			text_marker.pose.position.z = 1.05
-			text_marker.ns = area + "text"
-			text_marker.type = 9
-			text_marker.scale.z = 0.075
-			text_marker.color= color
-			text_marker.text = area
-			pub.publish(text_marker)
+				# text
+				text_marker = Marker()
+				text_marker.header.frame_id = "/base_link"
+				text_marker.header.stamp = rospy.Time.now()
+				text_marker.pose = get_pose_from_parameter_server(area).pose
+				text_marker.pose.position.z = 1.05
+				text_marker.ns = area + "text"
+				text_marker.type = 9
+				text_marker.scale.z = 0.075
+				text_marker.color= color
+				text_marker.text = area
+				pub.publish(text_marker)
 
-			# change color for next marker
-			color.g -= 0.1
+				# change color for next marker
+				color.g -= 0.1
 
-		rospy.sleep(1)
+			rospy.sleep(1)
+		except:
+			pass
 
 if __name__ == '__main__':
 	try:
