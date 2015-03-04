f8d50ca874cebe7621ea899505def74c2e153d93
ipa-fxm <felix.messmer@ipa.fhg.de>
2015-03-04 16:01:37 +0100

    setup updates

--- ipa_seminar_manipulation/setup_env_manipulation.bash
+++ ipa_seminar_manipulation/setup_env_manipulation.bash
@@ -2,6 +2,7 @@
 
 ### ROS
 #source /opt/ros/indigo/setup.bash
+#source /home/ros/moveit/devel/setup.bash
 #source /home/ros/seminar/devel/setup.bash
 source ~/seminar/devel/setup.bash
 
@@ -14,5 +15,5 @@ export ROS_MASTER_URI=http://localhost:11311
 alias group='source ~/seminar/devel/setup.bash'
 alias solution='source /home/ros/seminar/devel/setup.bash'
 
-alias ssh-robot='ssh -X ros@pyramid-2'
-alias exp-robot='export ROS_MASTER_URI=http://pyramid-2:11311'
+#alias ssh-robot='ssh -X ros@pyramid-2'
+#alias exp-robot='export ROS_MASTER_URI=http://pyramid-2:11311'
